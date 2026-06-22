1be99ea6aa8685c8c588f5b12f77528f5f82db02
Bitangsa Saha <bitangsasaha75@gmail.com>
2026-06-22 09:47:44 +0000

    First push

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+
+  backend "s3" {
+
+    bucket         = "terraform-state-bitangsa"
+    key            = "ppr/s3-poc.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+
+    encrypt = true
+  }
+}
